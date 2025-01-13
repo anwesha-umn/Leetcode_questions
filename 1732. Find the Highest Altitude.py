f690e7af71a1580782cd5a8e98c1dc0d7a62043d
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-01-13 07:47:47 +0530

    Update 1732. Find the Highest Altitude.py

--- 1732. Find the Highest Altitude.py
+++ 1732. Find the Highest Altitude.py
@@ -10,11 +10,13 @@ Example 1:
 Input: nums = [1,1,0,1]
 Output: 3
 Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+
 Example 2:
 
 Input: nums = [0,1,1,1,0,1,1,0,1]
 Output: 5
 Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+
 Example 3:
 
 Input: nums = [1,1,1]
