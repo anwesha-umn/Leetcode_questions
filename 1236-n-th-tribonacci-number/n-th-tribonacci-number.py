a013fdb58d6ab67d013b71a04abbbf6f03e2af37
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-01-19 18:29:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (6.78%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -4,25 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-
-        if n==0:
+        if n == 0:
             return 0
-        elif n==1 or n==2:
+        elif n == 1 or n == 2:
             return 1
-
         else:
             t0 = 0
             t1 = 1
             t2 = 1
-            
-            for i in range(3,n+1):
+
+            for i in range(3, n+1):
                 t3 = t0 + t1 + t2
                 t0 = t1
                 t1 = t2
                 t2 = t3
-                
-        return t3
 
-        
-        
-        
+        return t3
