3e080ad92b98214af8c5382d82560624c0d029eb
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-01-20 06:25:34 -0600

    Time: 7 ms (30.99%) | Memory: 14 MB (5.51%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,19 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # k = 1
-        # for i in range(1, len(nums)):
-        #     if nums[i] != nums[i-1]:
-        #         nums[k] = nums[i]
-        #         k += 1
+        #method 1
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
  
+        return k
 
-        # return k
-
-        set_nums = sorted(list(set(nums)))
+        #method 2
+        # set_nums = sorted(list(set(nums)))
         
-        for i in range(len(set_nums)):
-            nums[i]=set_nums[i]
+        # for i in range(len(set_nums)):
+        #     nums[i]=set_nums[i]
         
-        return len(set_nums)
+        # return len(set_nums)
         
