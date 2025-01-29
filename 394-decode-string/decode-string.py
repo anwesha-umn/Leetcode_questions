7ac6e98103fd731ead340812d540adf7e489bc9d
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-01-28 21:35:08 -0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (38.74%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+
+        i = 0
+        num = 0
+        temp = ""
+        res = []
+
+        for char in s:
+            if char.isdigit():
+                num = num*10+int(char)
+
+            elif char == "[":
+                res.append((temp, num))
+                temp = ""
+                num = 0
+            
+            elif char == "]":
+                string, nums = res.pop()
+                temp = string + temp*nums
+            else:
+                temp += char
+
+        return temp
+                
+                
+                    
+
+                
+
