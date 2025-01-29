487d28f0339427216f3ebe633f27f89c88f690fe
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-01-28 21:44:21 -0600

    Time: 7 ms (1.95%) | Memory: 12.3 MB (60.66%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -6,26 +6,30 @@ class Solution(object):
         """
         
         num = 0
+        stack = [] # LIFO
+        num = 0
         temp = ""
-        res = []
 
         for char in s:
+
             if char.isdigit():
-                num = num*10 + int(char)
+                num = num * 10 + int(char)
 
             elif char == "[":
-                res.append((temp, num))
+                stack.append((temp, num))
                 temp = ""
                 num = 0
-            
+
             elif char == "]":
-                string, nums = res.pop()
-                temp = string + temp*nums
+                string, nums = stack.pop()
+                temp = string + temp * nums
+
             else:
                 temp += char
 
+            print(stack)
+
         return temp
-                
                 
                     
 
