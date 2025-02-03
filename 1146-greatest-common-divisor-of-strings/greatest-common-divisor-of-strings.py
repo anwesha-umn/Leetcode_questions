155093e4cce25c735622c6bcba529fb0a12a7772
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-02-02 18:23:57 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (48.32%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+
+        def gcd(a,b):
+            while b!=0:
+                a, b = b, a % b
+
+            return a
+
+        str3 = ""
+        gcd_len = gcd(len(str1), len(str2))
+
+        str3 = str1[:gcd_len]
+
+        if str3*int(len(str1) // gcd_len) == str1 and str3*int(len(str2) // gcd_len) == str2:
+            return str3
+        else:
+            return ""
+        
