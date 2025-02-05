f41b34823992f924a02188613898b5cd14784404
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-02-04 20:40:34 -0600

    Time: 1041 ms (5.00%) | Memory: 25.6 MB (40.11%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,22 +1,13 @@
-class Solution(object):
-    def rotate(self, nums, k):
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-
-        # get the start index number in rotated array
-        n = len(nums)
-        
-        # if k is > len(nums)
-        k = k % n      
-        rotated = [0] * n
-
-        for i in range(n):
-            rotated[(i + k) % n] = nums[i]
-
-        for i in range(n):
-            nums[i] = rotated[i]
+        i = 0
+        new = []
+        while i < k:
+            temp = nums.pop()
+            nums.insert(0,temp)
+            i+=1
 
        
