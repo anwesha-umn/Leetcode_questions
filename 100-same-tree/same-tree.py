4990fca5a09edce4293136cce7c370724b59f907
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-02-11 21:25:18 -0600

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.02%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -7,13 +7,13 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
        
-        # DFS - RECURSION
-        if not p and not q:
+        # DFS - RECURSION - O(N)
+        if not p and not q:   # if both nodes are null - return True
             return True
 
         if p and q and p.val == q.val:   # if root values are same go to child nodes
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-       
+        # if above 2 conditions are not True
         return False
 
