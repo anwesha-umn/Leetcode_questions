fba2c49f0060f583475fb53cb5c962d503bd8c34
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-02-19 21:13:46 -0600

    Time: 28 ms (17.33%) | Memory: 17.8 MB (96.51%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,22 +2,19 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
         mag_dict = {}
-        ran_dict = {}
+        
+        for char in magazine:
+            if char not in mag_dict:
+                mag_dict[char]=1
+            else:
+                mag_dict[char]+=1
 
-        for i in range(len(ransomNote)):
-            if ransomNote[i] not in ran_dict:
-                ran_dict[ransomNote[i]]=1
-            else:
-                ran_dict[ransomNote[i]]+=1
-        
-        for i in range(len(magazine)):
-            if magazine[i] not in mag_dict:
-                mag_dict[magazine[i]]=1
-            else:
-                mag_dict[magazine[i]]+=1
-        
-        for key, val in ran_dict.items():
-            if key not in mag_dict.keys() or mag_dict[key] < val:
+        for char in ransomNote:
+            if char not in mag_dict or mag_dict[char] <= 0:
                 return False
+
+            else:
+                mag_dict[char] -= 1 
+       
 
         return True
