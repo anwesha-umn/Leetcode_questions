e911cad0ea7fd9ff49c9e6f785a416546ba4f735
Anwesha Samaddar <39149911+anwesha-umn@users.noreply.github.com>
2025-02-21 22:00:33 -0600

    Time: 7 ms (57.21%) | Memory: 17.9 MB (79.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,25 +1,25 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
-        s_to_t = {}
-        t_to_s = {}
+        # two dictionary of mappings 
+        # map s to t
+        s_t = {}
+
+        # map t to s
+        t_s = {}
 
         for i in range(len(s)):
-            if s[i] not in s_to_t:
-                s_to_t[s[i]] = t[i] 
+            if s[i] not in s_t:
+                s_t[s[i]] = t[i]
+
             else:
-                if s_to_t[s[i]] != t[i]:
+                if s_t[s[i]] != t[i]:
                     return False
 
-            if t[i] not in t_to_s:
-                t_to_s[t[i]] = s[i]
+            if t[i] not in t_s:
+                t_s[t[i]] = s[i]
             else:
-                if t_to_s[t[i]] != s[i]:
+                if t_s[t[i]]!=s[i]:
                     return False
-
-
-
-
 
         return True
-                
